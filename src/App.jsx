caf1e1961480f600b607c3e9d1d476da114a0fb2
unknown <you@example.com>
2025-12-04 01:34:33 +0800

    Added clickcounter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from "react";
+import Counter from "./components/Counter.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Mini Project: Click Counter</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,23 @@
+// Counter.jsx
+import React, { useState } from "react";
+import "./counter.css";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="counter-card">
+      <h2>Click Counter</h2>
+      <p className="count-display">Count: {count}</p>
+
+      <button
+        className="counter-btn"
+        onClick={() => setCount(count + 1)}
+      >
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
